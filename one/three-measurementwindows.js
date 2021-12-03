1df6567094e95e35dafb3739fb4f3582d0b0c29c
Brndndev <brndndev@outlook.com>
2021-12-03 16:00:29 -0500

    initial commit: added ✅ day 1 and ⚪ day 2 (not started)

---
+++ one/three-measurementwindows.js
@@ -0,0 +1,11 @@
+//count the NUMBER OF TIMES 
+//the SUM OF MEASUREMENTS in this
+// sliding window increases from the 
+// previous sum.
+
+
+// COMPARE [a] with [b]
+
+// then COMPARE [b] with [c]
+
+// then [c] with [d] and so on
